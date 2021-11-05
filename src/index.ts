02232e6ea6b074a137515e1796d55f7c2b3d6cd3
Cytaa <piotrstachnio@gmail.com>
2021-11-05 21:13:23 +0100

    set up basic function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-const chuj = () => {
-    console.log("sieman1");
-};
+const { Client, Intents } = require("discord.js");
+const dotenv = require("dotenv");
 
-chuj();
+dotenv.config();
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once("ready", () => {
+    console.log("Ready!");
+});
+
+client.login(process.env.TOKEN);
